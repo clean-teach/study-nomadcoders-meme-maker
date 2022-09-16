d7156071b741d556c17a944b4d4f3861fd2e80c5
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-09-17 01:20:31 +0900

    #2.3 Paint Color part One
    - **목표 :**
        - 색상 설정을 구현해본다.
    - **구현 :**
        - color input 태그를 설정 인터페이스로 사용

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const lineWidth = document.querySelector('#line-width');
+const color = document.querySelector('#color');
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
@@ -26,6 +27,10 @@ const cancelPainting = () => {
 const onLineWidthChange = event => {
     ctx.lineWidth = event.target.value;
 };
+const onColorChange = event => {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+};
 
 canvas.addEventListener("mousemove", handleMouseMove);
 canvas.addEventListener("mousedown", startPainting);
@@ -33,3 +38,4 @@ document.addEventListener("mouseup", cancelPainting);
 // canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener('change', onLineWidthChange);
+color.addEventListener('change', onColorChange);
